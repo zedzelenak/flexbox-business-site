91bba279475ea0ac39b5da2823f01c63b8fde320
Matt Zelenak <matt@zelenak.me>
2022-07-08 12:51:54 -0400

    start of mobile nav toggle

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+window.addEventListener('load', () => {
+  function isHidden(el) {
+    return (el.offsetParent === null)
+  }
+  
+  document.querySelector('#nav-toggle').addEventListener('click', () => {
+    const mobileNav = document.querySelector('header nav');
+    mobileNav.style.display = isHidden(mobileNav) ? 'flex' : 'none';
+  });
+}, false);
